942a3d73eab1e5297d1580c56ef6b49a1c23560e
Gerard Dróżdż <gerardd@interia.pl>
2021-04-25 04:44:40 +0200

    Main stub, reading system args

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import           System.Environment
+import           System.Exit
+import           System.IO
+
+import           Parser.Tidy.Abs
+import           Parser.Tidy.Lex
+import           Parser.Tidy.Par
+
+usage :: IO ()
+usage = putStrLn "Usage: ./tidy source_file_path"
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case args of
+        []           -> usage
+        [sourceFile] -> putStrLn "TODO"
+        _            -> usage
