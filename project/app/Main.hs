d994a1b0a75095919cf0175c26478df95fcb4245
Gerard Dróżdż <gerardd@interia.pl>
2021-04-25 05:28:08 +0200

    Changes in project structure and scripts

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -1,12 +1,38 @@
 module Main where
 
-import           System.Environment
-import           System.Exit
+import           System.Directory   (doesFileExist)
+import           System.Environment (getArgs)
+import           System.Exit        (exitFailure)
 import           System.IO
 
 import           Parser.Tidy.Abs
 import           Parser.Tidy.Lex
 import           Parser.Tidy.Par
+
+
+lexer :: String -> [Token]
+lexer = myLexer
+
+parser :: [Token] -> Either String Program
+parser = pProgram
+
+
+interpret :: Program -> IO ()
+interpret astTree = putStrLn "TODO"
+
+
+interpretFile :: String -> IO ()
+interpretFile filePath = do
+    fileExists <- doesFileExist filePath
+    if fileExists then do
+        source <- readFile filePath
+        case (parser . lexer) source of
+            Left error -> do
+                hPutStrLn stderr $ "Error: parsing failed: " ++ error
+                exitFailure
+            Right astTree -> interpret astTree
+    else do
+        hPutStrLn stderr $ "Error: file " ++ filePath ++ " does not exist."
 
 usage :: IO ()
 usage = putStrLn "Usage: ./tidy source_file_path"
@@ -16,5 +42,5 @@ main = do
     args <- getArgs
     case args of
         []           -> usage
-        [sourceFile] -> putStrLn "TODO"
+        [sourceFile] -> interpretFile sourceFile
         _            -> usage
