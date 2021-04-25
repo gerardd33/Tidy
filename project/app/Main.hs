cd37e4fbb7d8e8684660daca3be44b86d6a2f089
Gerard Dróżdż <gerardd@interia.pl>
2021-04-25 05:56:39 +0200

    Created Interpreter.Main module

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -5,6 +5,7 @@ import           System.Environment (getArgs)
 import           System.Exit        (exitFailure)
 import           System.IO
 
+import           Interpreter.Main
 import           Parser.Tidy.Abs
 import           Parser.Tidy.Lex
 import           Parser.Tidy.Par
@@ -17,29 +18,26 @@ parser :: [Token] -> Either String Program
 parser = pProgram
 
 
-interpret :: Program -> IO ()
-interpret astTree = putStrLn "TODO"
-
-
 interpretFile :: String -> IO ()
 interpretFile filePath = do
     fileExists <- doesFileExist filePath
-    if fileExists then do
+    if not fileExists then do
+        hPutStrLn stderr $ "Error: file " ++ filePath ++ " does not exist."
+    else do
         source <- readFile filePath
         case (parser . lexer) source of
             Left error -> do
-                hPutStrLn stderr $ "Error: parsing failed: " ++ error
+                hPutStrLn stderr $ "Error: " ++ error
                 exitFailure
             Right astTree -> interpret astTree
-    else do
-        hPutStrLn stderr $ "Error: file " ++ filePath ++ " does not exist."
+
 
 usage :: IO ()
 usage = putStrLn "Usage: ./tidy source_file_path"
 
 main :: IO ()
 main = do
-    -- Changing the default directory to Tidy project root instead of the stack project root
+    -- Changing the default directory to Tidy project root instead of the stack project root.
     setCurrentDirectory ".."
     args <- getArgs
     case args of
---
+++ project/src/Interpreter/Main.hs
@@ -0,0 +1,6 @@
+module Interpreter.Main where
+
+import           Parser.Tidy.Abs
+
+interpret :: Program -> IO ()
+interpret astTree = putStrLn "TODO Interpret"
