50e84c8d7dbaec3f33793444d31d9dcb5417da19
Gerard Dróżdż <gerardd@interia.pl>
2021-05-01 02:27:49 +0200

    First program with basic expression is properly executed

--- project/src/Interpreter/Runtime.hs
+++ project/src/Interpreter/Runtime.hs
@@ -4,26 +4,64 @@ import           Control.Monad.Except
 import           Control.Monad.Reader
 import           Control.Monad.State
 import qualified Data.Map                as Map
+import           Data.Maybe
 import           System.IO
 
-import           Commons
+import           Interpreter.Classes
+import           Interpreter.Commons
 import           Interpreter.Environment
+import           Interpreter.Functions
 import           Parser.Tidy.Abs
 
 
+-- TODO put mode in the env or somewhere
 -- TODO for now a POC
 -- TODO there should be something more to execute passed besides ClassEnv
 runtime :: Mode -> ClassEnv -> ClassDecl -> IO (Either RuntimeException Result)
 runtime mode classEnv mainClass = runExceptT $ evalStateT (runReaderT (runtimeBody mode classEnv mainClass) Map.empty) (Map.empty, 0)
 
+-- TODO later put classEnv in the env or do something with it
 runtimeBody :: Mode -> ClassEnv -> ClassDecl -> StateMonad Result
 runtimeBody mode classEnv mainClass = do
     liftIO $ debugLog mode "Runtime..."
     -- TODO initial environment and state updates
-    env <- ask
-    return (Nothing, env)
+    evalAction $ fromJust $ getMainAction mainClass
 
--- TODO POC, very simple
-declareValue :: Identifier -> Integer -> StateMonad Result
-declareValue identifier valueExpr = do
-    addValue identifier (IntValue valueExpr)
+-- TODO passing parameters and other context information
+evalAction :: ActionDecl -> StateMonad Result
+evalAction action = do
+    evalActionBody (getActionBody action)
+
+evalActionBody :: ActionBody -> StateMonad Result
+evalActionBody (ActionBodyOneLine expr)    = evalExpressionList [expr]
+evalActionBody (ActionBodyMultiLine exprs) = evalExpressionList exprs
+
+evalExpressionList :: [Expr] -> StateMonad Result
+evalExpressionList [expr] = evalExpr expr
+evalExpressionList (expr:exprs) = do
+    (_, env) <- evalExpr expr
+    local (const env) (evalExpressionList exprs)
+
+evalExpr :: Expr -> StateMonad Result
+evalExpr (ELiteral literal) = do
+    result <- evalLiteral literal
+    env <- ask
+    return (Just result, env)
+evalExpr (ELocalValue identifier) = do
+    env <- ask
+    value <- getValue identifier
+    return (Just value, env)
+evalExpr (ELocalValueDecl (LocalVDecl (PublicValueDecl decl))) =
+    declareValue decl
+
+evalLiteral :: Literal -> StateMonad Value
+evalLiteral (LInt int)   = return (IntValue int)
+evalLiteral (LBool bool) = return (BoolValue bool)
+evalLiteral (LVoid void) = return VoidValue
+-- TODO char, string
+
+declareValue :: ValueDeclProper -> StateMonad Result
+declareValue (InitialisedValue identifier valueType expr) = do
+    (initializationValue, _) <- evalExpr expr
+    addValue identifier $ fromJust initializationValue
+-- TODO change from int to any type
