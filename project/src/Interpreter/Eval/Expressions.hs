7b3c7df918ccf6ab66ab61361a5ffefafe03ca08
Gerard Dróżdż <gerardd@interia.pl>
2021-05-04 01:41:42 +0200

    Implemented evaluation of all remaining operators

--- project/src/Interpreter/Eval/Expressions.hs
+++ project/src/Interpreter/Eval/Expressions.hs
@@ -27,8 +27,11 @@ evalExpr (EAdd expr1 expr2) = evalBinaryOperator expr1 expr2 evalAddition
 evalExpr (ESubtract expr1 expr2) = evalBinaryOperator expr1 expr2 evalSubtraction
 evalExpr (EMultiply expr1 expr2) = evalBinaryOperator expr1 expr2 evalMultiplication
 evalExpr (EDivide expr1 expr2) = evalBinaryOperator expr1 expr2 evalMultiplication
+evalExpr (EConcatenate expr1 expr2) = evalBinaryOperator expr1 expr2 evalConcatenation
 evalExpr (EUnaryNot expr) = evalUnaryOperator expr evalUnaryNot
 evalExpr (EUnaryMinus expr) = evalUnaryOperator expr evalUnaryMinus
+evalExpr (ERelationalOperator expr1 operator expr2) = evalRelationalOperator expr1 expr2 operator
+evalExpr (EBooleanOperator expr1 operator expr2) = evalBooleanOperator expr1 expr2 operator
 
 evalExpr (ECtorCall (CCall classIdentifier args)) = do
     env <- ask
@@ -70,10 +73,14 @@ evalMultiplication :: Value -> Value -> StateMonad Value
 evalMultiplication (SingleValueObject (IntValue v1)) (SingleValueObject (IntValue v2)) =
     return (newSingleValueObject $ IntValue $ v1 * v2)
 
--- -- TODO throw if division by zero
+-- TODO throw if division by zero
 evalDivision :: Value -> Value -> StateMonad Value
 evalDivision (SingleValueObject (IntValue v1)) (SingleValueObject (IntValue v2)) =
     return (newSingleValueObject $ IntValue $ v1 `div` v2)
+
+evalConcatenation :: Value -> Value -> StateMonad Value
+evalConcatenation (SingleValueObject (StringValue v1)) (SingleValueObject (StringValue v2)) =
+    return (newSingleValueObject $ StringValue $ v1 ++ v2)
 
 evalUnaryNot :: Value -> StateMonad Value
 evalUnaryNot (SingleValueObject (BoolValue BTrue)) = return (newSingleValueObject $ BoolValue BFalse)
@@ -81,6 +88,35 @@ evalUnaryNot (SingleValueObject (BoolValue BFalse)) = return (newSingleValueObje
 
 evalUnaryMinus :: Value -> StateMonad Value
 evalUnaryMinus (SingleValueObject (IntValue value)) = return (newSingleValueObject $ IntValue $ -value)
+
+evalRelationalOperator :: Expr -> Expr -> RelationalOperator -> StateMonad Result
+evalRelationalOperator expr1 expr2 RLess = evalBinaryOperator expr1 expr2 (evalRelational (<))
+evalRelationalOperator expr1 expr2 RLessEqual = evalBinaryOperator expr1 expr2 (evalRelational (<=))
+evalRelationalOperator expr1 expr2 RGreater = evalBinaryOperator expr1 expr2 (evalRelational (>))
+evalRelationalOperator expr1 expr2 RGreaterEqual = evalBinaryOperator expr1 expr2 (evalRelational (>=))
+evalRelationalOperator expr1 expr2 REqual = evalBinaryOperator expr1 expr2 evalEquality
+evalRelationalOperator expr1 expr2 RNotEqual = evalBinaryOperator expr1 expr2 evalNonEquality
+
+evalRelational :: (Integer -> Integer -> Bool) -> Value -> Value -> StateMonad Value
+evalRelational operator (SingleValueObject (IntValue v1)) (SingleValueObject (IntValue v2)) =
+    return (newSingleValueObject $ BoolValue $ toBoolean $ operator v1 v2)
+
+evalBooleanOperator expr1 expr2 BAnd = evalBinaryOperator expr1 expr2 evalBooleanAnd
+evalBooleanOperator expr1 expr2 BOr = evalBinaryOperator expr1 expr2 evalBooleanOr
+
+evalBooleanAnd :: Value -> Value -> StateMonad Value
+evalBooleanAnd (SingleValueObject (BoolValue v1)) (SingleValueObject (BoolValue v2)) =
+    return (newSingleValueObject $ BoolValue $ toBoolean $ fromBoolean v1 && fromBoolean v2)
+
+evalBooleanOr :: Value -> Value -> StateMonad Value
+evalBooleanOr (SingleValueObject (BoolValue v1)) (SingleValueObject (BoolValue v2)) =
+    return (newSingleValueObject $ BoolValue $ toBoolean $ fromBoolean v1 || fromBoolean v2)
+
+evalEquality :: Value -> Value -> StateMonad Value
+evalEquality v1 v2 = return (newSingleValueObject $ BoolValue $ toBoolean $ v1 == v2)
+
+evalNonEquality :: Value -> Value -> StateMonad Value
+evalNonEquality v1 v2 = return (newSingleValueObject $ BoolValue $ toBoolean $ v1 /= v2)
 
 declareValue :: ValueDeclProper -> StateMonad Result
 declareValue (InitializedValue identifier valueType expr) = do
@@ -104,3 +140,11 @@ evalAttributeExpressions attributeList = do
     let (names, exprs) = unzip attributeList
     evalResults <- mapM evalExpr exprs
     return $ zip names (map fst evalResults)
+
+toBoolean :: Bool -> Boolean
+toBoolean True  = BTrue
+toBoolean False = BFalse
+
+fromBoolean :: Boolean -> Bool
+fromBoolean BTrue  = True
+fromBoolean BFalse = False
