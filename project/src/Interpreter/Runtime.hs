72128b37499cad12398ce69e450846b78a09560c
Gerard Dróżdż <gerardd@interia.pl>
2021-05-03 19:30:27 +0200

    Return only result value, not env from runtime

--- project/src/Interpreter/Runtime.hs
+++ project/src/Interpreter/Runtime.hs
@@ -4,6 +4,7 @@ import           Control.Monad.Except
 import           Control.Monad.Reader
 import           Control.Monad.State
 import           Data.Maybe
+import           Data.Tuple
 
 import           Interpreter.Common.Errors
 import           Interpreter.Common.Types
@@ -14,12 +15,13 @@ import           Interpreter.Eval.Environment
 import           Parser.Tidy.Abs
 
 
-runtime :: Mode -> ClassEnv -> ClassDecl -> IO (Either RuntimeException Result)
+runtime :: Mode -> ClassEnv -> ClassDecl -> IO (Either RuntimeException Value)
 runtime mode classEnv mainClass = runExceptT $ evalStateT
     (runReaderT (runtimeBody mode classEnv mainClass) (buildInitialEnv classEnv)) buildInitialState
 
-runtimeBody :: Mode -> ClassEnv -> ClassDecl -> StateMonad Result
+runtimeBody :: Mode -> ClassEnv -> ClassDecl -> StateMonad Value
 runtimeBody mode classEnv mainClass = do
     liftIO $ debugLog mode "Runtime..."
     -- TODO initial environment and state updates
-    evalAction $ fromJust $ getMainAction mainClass
+    result <- evalAction $ fromJust $ getMainAction mainClass
+    return $ fst result
